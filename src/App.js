444b741804d6a98630e944e92a566958041a9aa3
Vinicius Uliano <vgarciauliano@gmail.com>
2024-05-22 21:12:39 -0300

    praticandocomreact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./index.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[nutri, setNutri] = useState([])
+
+  useEffect(()=>{
+    function readApi(){
+    
+      fetch("https://sujeitoprogramador.com/rn-api/?api=posts")
+      .then((r)=> r.json())
+      .then((json)=>{
+      console.log(json)
+      setNutri(json)
+      })
+    }
+    readApi()
+
+  }, [])    
+  return (  
+    <div>
+      <header className="cabecalho">Dicas Nutri React</header>
+      {nutri.map((item)=>{
+        return(
+          <article key={item.id} className="article">
+            <strong>{item.titulo}</strong>
+            <img alt="capa" src={item.capa}/>
+            <p>{item.subtitulo}</p>
+            <strong>{item.categoria}</strong>
+          </article>
+)
+      })}
     </div>
+
   );
 }
 
